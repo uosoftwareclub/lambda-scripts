7c47f073cf01b85d17e0fadccacb2a66c2c47a6f
David Hew-wing <dhew67@gmail.com>
2020-03-22 15:56:13 -0400

    added profile scraper

--- src/profile_scraper/user_info.py
+++ src/profile_scraper/user_info.py
@@ -2,13 +2,12 @@ USERNAMES = [
     'skrj', #Saibi Boghal
     'kiluam87', #Maulik Shah
     'melkman', #Max Melkonian
-    'atronova', #Patrick Nyugen
+    'astronova', #Patrick Nyugen
     'andrewkp', #Andrew Pratheepan
     '0x77756f6e', #Daniel Wu
     'l1ghtspeed', #Zi Gao (main)
     'helloworldguys', #David Hew-wing (smurf)
     'techwithtimm', #Tim Ruscica
-    '0x00011100', # Zi Gao (smurf)
     'aaoun723', #Anthony Aoun,
     'Ced2142' #Cedric Brisebois
 ]
